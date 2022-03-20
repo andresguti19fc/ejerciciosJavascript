b2c1f45795a1745e98069da2a5dc562ebf15703e
andresguti19fc <chavogutierrez19@gmail.com>
2022-03-20 10:56:27 -0300

    desafio listo

--- main.js
+++ main.js
@@ -1,12 +1,22 @@
 let numero = prompt("ingresa cuantos litros de nafta necesitas");
 let i = 0;
+let precioNafta = parseInt(110);
+let resultado = 0;
 while (i != numero) {
-  
+  i++;
+
   if (numero <= 0 || isNaN(numero)) {
     numero = prompt(`"${numero}" no es un numero valido.
       ingresa cuantos litros de nafta necesitas`);
   } else {
-    document.write(`usted acaba de comprar ${numero} litros de nafta. muchas gracias por su compra`);
-    break;
+    resultado = i * precioNafta;
+    if (i != numero) {
+      continue;
+    }
+
+    document.write(`${i} lts son = ${resultado} $ <br>`);
   }
 }
+document.write(
+  `usted acaba de comprar ${numero} litros de nafta. muchas gracias por su compra`
+);
